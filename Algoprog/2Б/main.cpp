dbf079f01d943575546509ae4dd93a5c6ab70780
= <jeka.sudakov@mail.ru>
2020-08-13 09:28:23 +0300

    11.08.2020

--- Algoprog/2Б/main.cpp
+++ Algoprog/2Б/main.cpp
@@ -6,10 +6,14 @@
 
 using namespace std;
 
+
 int32_t main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
+    int n;
+    cin >> n;
 
 
 
--- Algoprog/2Б/Множители/Разложение на простые множители.cpp
+++ Algoprog/2Б/Множители/Разложение на простые множители.cpp
@@ -1,4 +1,44 @@
 //
-// Created by yudjin on 09.08.2020.
+// Created by yudjin on 11.08.2020.
 //
+
+#include <bits/stdc++.h>
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+
+using namespace std;
+
+int least_divizor(int x) {
+    for(int i = 2; i*i <=x; i++) {
+        if(x % i == 0) return i;
+    }
+    return x;
+}
+
+void factor(int x) {
+    while(x > 1) {
+        int div = least_divizor(x);
+        x /= div;
+        cout << div;
+        if(x != 1) {
+            cout << "*";
+        }
+    }
+    cout << "\n";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int x;
+    cin >> x;
+    factor(x);
+
+
+    return 0;
+}
 
